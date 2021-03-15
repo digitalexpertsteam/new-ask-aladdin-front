ba6442331d5d83f84da20d0ce5c8ac9c7113fbc4
ahmedadel101 <59322068+ahmedadel101@users.noreply.github.com>
2021-03-15 15:40:22 +0200

    update single destination

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,15 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  languages= [ 'En' , 'Ru' , 'Es' , 'De' , 'Fr']
-  langText = ''
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onClick(i:any){
-    this.langText = i.text
-  }
+  
 }
 
