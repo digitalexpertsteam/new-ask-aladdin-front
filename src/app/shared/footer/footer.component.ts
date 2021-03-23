13173501853894c16f39f7a340df974c64994aeb
ahmedadel101 <59322068+ahmedadel101@users.noreply.github.com>
2021-03-23 17:10:14 +0200

    add fotter

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { destination } from '../../interfaces/destination';
 import { socials } from '../../interfaces/socials';
 import { HomeserviceService } from '../../services/homeservice.service';
 
@@ -10,13 +11,28 @@ import { HomeserviceService } from '../../services/homeservice.service';
 export class FooterComponent implements OnInit {
 
   isDropup = true;
-
+  destinationContainer:destination[]=[];
+  packagesName:any;
+  guidesSlug:destination[]=[];
   date = new Date();
   socialsContainer:socials[] = []
-  constructor(private _socials:HomeserviceService) { }
+  constructor(private _footer:HomeserviceService) { }
 
   ngOnInit(): void {
-    this._socials.getSocials().subscribe(result => this.socialsContainer = result.data)
-  }
+    this._footer.getSocials().subscribe(result => this.socialsContainer = result.data)
 
+    this._footer.getAlldestination().subscribe(result => this.destinationContainer = result.data)
+    
+    this._footer.getOneDestinationContent().subscribe(result => {
+      this.packagesName = result.data[0].page_content[1]
+      this.guidesSlug = result.data[0].page_content[0]
+      
+      
+    })
+  }
+  setId(id:any){
+    localStorage.setItem("id" , id)
+    console.log(id);
+    
+  }
 }
