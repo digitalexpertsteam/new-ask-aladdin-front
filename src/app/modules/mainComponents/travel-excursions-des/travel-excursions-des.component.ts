2efbd69c2bd64884e8eb31a2473afa017d0bfab3
ahmedadel101 <59322068+ahmedadel101@users.noreply.github.com>
2021-03-31 09:48:49 +0200

    aa

--- src/app/modules/mainComponents/travel-excursions-des/travel-excursions-des.component.ts
+++ src/app/modules/mainComponents/travel-excursions-des/travel-excursions-des.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HomeserviceService } from '../../../services/homeservice.service';
 
 @Component({
   selector: 'app-travel-excursions-des',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelExcursionsDesComponent implements OnInit {
 
-  constructor() { }
+
+
+  max = 5;
+  isReadonly = false;
+  // duration_in_days;
+  num : any;
+  night:any=[];
+  overStar: number | undefined;
+  percent: number | undefined;
+  hoveringOver(value: number): void {
+    this.overStar = value;
+    this.percent = (value / this.max) * 100;
+  }
+  id:any;
+  idpackage:any
+  getDestinationExcursions:Excursions[] = []
+
+  constructor(private _excursions:HomeserviceService, private sanitizer:DomSanitizer) { }
 
   ngOnInit(): void {
+    this.id = localStorage.getItem("id")
+    this._excursions.getDestinationExcursions(this.id).subscribe(result =>{
+      this.getDestinationExcursions = result.data
+      console.log(this.getDestinationExcursions);
+      
+      
+    })
   }
-
+  transform(url:any) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+  setId(id:any){
+    localStorage.setItem('idPack' , id)
+    
+    
+  }
+  
 }
